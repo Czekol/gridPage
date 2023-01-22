cbe38bcf78c82fb2e076d6bc3d235fd6e32016e7
Bartosz <bartoszweb8@gmail.com>
2023-01-23 00:12:40 +0100

    finished all website :D

--- js/main.js
+++ js/main.js
@@ -1,18 +1,28 @@
 const navMobile = document.querySelector('.nav-mobile')
 const navBtn = document.querySelector('.hamburger')
+const navLinks = document.querySelectorAll('.nav__link')
+const body = document.querySelector('body')
 const footerYear = document.querySelector('.footer__year')
 
-const handleNav = (params) => {
-    navBtn.classList.toggle('is-active')
-    navMobile.classList.toggle('nav-mobile--active')
+const handleNav = params => {
+	navBtn.classList.toggle('is-active')
+	navMobile.classList.toggle('nav-mobile--active')
+	body.classList.toggle('stop-scrool')
+
+	navLinks.forEach(link => {
+		link.addEventListener('click', () => {
+			body.classList.remove('stop-scrool')
+            navBtn.classList.remove('is-active')
+            navMobile.classList.remove('nav-mobile--active')
+		})
+	})
 }
 
 const handleCurrentYear = () => {
-    const year = new Date().getFullYear()
-    footerYear.textContent = year
+	const year = new Date().getFullYear()
+	footerYear.textContent = year
 }
 
 handleCurrentYear()
 
-navBtn.addEventListener('click' , handleNav)
-
+navBtn.addEventListener('click', handleNav)
